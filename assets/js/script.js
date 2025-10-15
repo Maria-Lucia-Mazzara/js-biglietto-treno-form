51324afc3e3602cb775e943bc340c94bd2a4c264
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-15 16:39:54 +0200

    aggiunti bottoni in html e fatta parte js dove inserisco le informazioni

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,14 @@ const input_name = document.querySelector(`name`);
 const input_km = document.querySelector(`km_fare`);
 const input_eta = document.querySelector(`fascia_eta`);
 
+// 
+myForm.addEventListener('submit', function (event) {
 
+    //blocco comportamento di default
+    event.preventDefault();
+
+    // leggo i valori inseriti dall'utente
+    const nome_utente = input_name.value;
+    const km_da_fare = input_km.value;
+    const eta_utente = input_eta.value;
+});
