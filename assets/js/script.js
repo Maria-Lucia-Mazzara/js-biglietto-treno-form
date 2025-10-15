e9efe3b873c0dc3448748c274810d4961c4806c1
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-15 16:15:53 +0200

    aggiunte le classi nel file js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
 console.dir(document);
 
+const myForm = document.getElementById(`Informazioni_viaggio`);
+const input_name = document.querySelector(`name`);
+const input_km = document.querySelector(`km_fare`);
+const input_eta = document.querySelector(`fascia_eta`);
+
+
