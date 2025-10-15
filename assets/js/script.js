a21456a924e62f8c9bec0070e547cfbcfe7cb6f0
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-15 17:30:42 +0200

    fatta parte delle condizioni  e aggiunto console.log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 console.dir(document);
 
-const myForm = document.getElementById(`Informazioni_viaggio`);
-const input_name = document.querySelector(`name`);
-const input_km = document.querySelector(`km_fare`);
-const input_eta = document.querySelector(`fascia_eta`);
+const myForm = document.getElementById(`informazioni_viaggio`);
+const input_name = document.querySelector(".name");
+const input_km = document.querySelector(".km_fare");
+const input_eta = document.querySelector(".fascia_eta");
 
-// 
+
+// inserisco dati utente
 myForm.addEventListener('submit', function (event) {
 
     //blocco comportamento di default
@@ -15,4 +16,21 @@ myForm.addEventListener('submit', function (event) {
     const nome_utente = input_name.value;
     const km_da_fare = input_km.value;
     const eta_utente = input_eta.value;
+
+    let ticket_cost = 0.21 * km_da_fare;
+
+    if (eta_utente === "minorenne") {
+        // sconto d'applicare agli under 18
+        ticket_cost = ticket_cost - ticket_cost * 20 / 100
+    } else if (eta_utente === "over65") {
+        // sconto d'applicare agli over 65
+        ticket_cost = ticket_cost - ticket_cost * 40 / 100
+    } 
+
+    console.log(`Nome passeggero: ${nome_utente}`);
+    console.log(`Km da fare: ${km_da_fare}`);
+    console.log(`Categoria età: ${eta_utente}`);
+    console.log(`Prezzo del biglietto ${ticket_cost.toFixed(2)}`);
 });
+
+
